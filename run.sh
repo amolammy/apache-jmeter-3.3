3f440459704c9b4fd6bc7f5dc31236ab069dff97
amolammy <32334946+amolammy@users.noreply.github.com>
2019-03-06 17:34:26 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env
-sh '/var/lib/git/apache-jmeter-3.3/bin/jmeter.sh -n -t /var/lib/git/apache-jmeter-3.3/bin/Pluto_rec_test.jmx -l test.jtl'
+sh bin/jmeter.sh -n -t bin/Pluto_rec_test.jmx -l test.jtl
 
